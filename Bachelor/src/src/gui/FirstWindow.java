a8dbb17db5f5c72f88ed9ee8d593d0a6ec0f2872
Mohamed Aboul Azm <mohamedtamer92@gmail.com>
2014-04-23 17:42:54 +0200

    loop successful, wrong drawings

--- Bachelor/src/src/gui/FirstWindow.java
+++ Bachelor/src/src/gui/FirstWindow.java
@@ -13,6 +13,7 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -24,8 +25,6 @@ public class FirstWindow {
 	GamePanel panel = new GamePanel();
 	JFrame guiFrame;
 
-	private boolean running = false;
-	private boolean paused = false;
 	private int fps = 60;
 	private int frameCount = 0;
 	boolean added = false;
@@ -36,7 +35,7 @@ public class FirstWindow {
 	public static void main(String[] args) {
 
 		FirstWindow window = new FirstWindow();
-		//window.run();
+		window.runGameLoop();
 	}
 
 	public FirstWindow()
@@ -55,62 +54,6 @@ public class FirstWindow {
 		guiFrame.setVisible(true);
 	}
 
-	public void run() {
-		int i, j;
-		//drawLanes();
-		ArrayList<Lane> lanes= new ArrayList<Lane>();
-		ArrayList<Car> cars= new ArrayList<Car>();
-		for(int counter = 0; counter < 10; counter ++) {
-			lanes = main.getLanes();
-			for( i = 0; i< lanes.size(); i++) {
-				this.lanes.get(i).removeAll();
-				cars = lanes.get(i).getCars();
-				for ( j = 0; j < cars.size(); j++) {
-					Insets insets = this.lanes.get(i).getInsets();
-					//g.fillRect(insets.left + 5*(lanes.get(i).getDistance() - cars.get(j).getCurrent_pos()) , insets.top,
-					//	cars.get(j).getCar_size()*5, this.lanes.get(i).getHeight());
-					//g.setColor(Color.red);
-					JLabel temp = new JLabel("Car");
-					temp.setForeground(Color.red);
-					temp.setBounds(insets.left + 5*(lanes.get(i).getDistance() - cars.get(j).getCurrent_pos()) , insets.top,
-							cars.get(j).getCar_size()*5, this.lanes.get(i).getHeight());
-					this.lanes.get(i).add(temp);
-				}
-			}
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			main.runSimulation();
-
-			lanes = main.getLanes();
-			for( i = 0; i< lanes.size(); i++) {
-				this.lanes.get(i).removeAll();
-				cars = lanes.get(i).getCars();
-				for ( j = 0; j < cars.size(); j++) {
-					Insets insets = this.lanes.get(i).getInsets();
-					//g.fillRect(insets.left + 5*(lanes.get(i).getDistance() - cars.get(j).getCurrent_pos()) , insets.top,
-					//	cars.get(j).getCar_size()*5, this.lanes.get(i).getHeight());
-					//g.setColor(Color.red);
-					JLabel temp = new JLabel("Car");
-					temp.setForeground(Color.red);
-					temp.setBounds(insets.left + 5*(lanes.get(i).getDistance() - cars.get(j).getCurrent_pos()) , insets.top,
-							cars.get(j).getCar_size()*5, this.lanes.get(i).getHeight());
-					this.lanes.get(i).add(temp);
-				}
-			}
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			main.cross();
-		}
-	}
-
 	public void runGameLoop()
 	{
 		Thread loop = new Thread()
@@ -122,19 +65,134 @@ public class FirstWindow {
 		};
 		loop.start();
 	}
+	public void gameLoop2() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while(true){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				updateGame();
+				updates++;
+				delta--;
+			}
+			drawGame(20);
+			frames++;
 
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames + " TICKS: " + updates);
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
 	public void gameLoop() {
+		//This value would probably be stored elsewhere.
+		final double GAME_HERTZ = 30.0;
+		//Calculate how many ns each frame should take for our target game hertz.
+		final double TIME_BETWEEN_UPDATES = 1000000000 / GAME_HERTZ;
+		//At the very most we will update the game this many times before a new render.
+		//If you're worried about visual hitches more than perfect timing, set this to 1.
+		final int MAX_UPDATES_BEFORE_RENDER = 5;
+		//We will need the last update time.
+		double lastUpdateTime = System.nanoTime();
+		//Store the last time we rendered.
+		double lastRenderTime = System.nanoTime();
 
+		//If we are able to get as high as this FPS, don't render again.
+		final double TARGET_FPS = 60;
+		final double TARGET_TIME_BETWEEN_RENDERS = 1000000000 / TARGET_FPS;
+
+		//Simple way of finding FPS.
+		int lastSecondTime = (int) (lastUpdateTime / 1000000000);
+		boolean running = false;
+		while (running)
+		{
+			double now = System.nanoTime();
+			int updateCount = 0;
+
+			//Do as many game updates as we need to, potentially playing catchup.
+			while( now - lastUpdateTime > TIME_BETWEEN_UPDATES && updateCount < MAX_UPDATES_BEFORE_RENDER )
+			{	
+				updateGame();
+				lastUpdateTime += TIME_BETWEEN_UPDATES;
+				updateCount++;
+			}
+
+			//If for some reason an update takes forever, we don't want to do an insane number of catchups.
+			//If you were doing some sort of game that needed to keep EXACT time, you would get rid of this.
+			if ( now - lastUpdateTime > TIME_BETWEEN_UPDATES)
+			{
+				lastUpdateTime = now - TIME_BETWEEN_UPDATES;
+			}
+
+			//Render. To do so, we need to calculate interpolation for a smooth render.
+			float interpolation = Math.min(1.0f, (float) ((now - lastUpdateTime) / TIME_BETWEEN_UPDATES) );
+			drawGame(interpolation);
+			lastRenderTime = now;
+
+			//Update the frames we got.
+			int thisSecond = (int) (lastUpdateTime / 1000000000);
+			if (thisSecond > lastSecondTime)
+			{
+				System.out.println("NEW SECOND " + thisSecond + " " + frameCount);
+				fps = frameCount;
+				frameCount = 0;
+				lastSecondTime = thisSecond;
+			}
+
+			//Yield until it has been at least the target time between renders. This saves the CPU from hogging.
+			while ( now - lastRenderTime < TARGET_TIME_BETWEEN_RENDERS && now - lastUpdateTime < TIME_BETWEEN_UPDATES)
+			{
+				Thread.yield();
+
+				//This stops the app from consuming all your CPU. It makes this slightly less accurate, but is worth it.
+				//You can remove this line and it will still work (better), your CPU just climbs on certain OSes.
+				//FYI on some OS's this can cause pretty bad stuttering. Scroll down and have a look at different peoples' solutions to this.
+				try {Thread.sleep(1);} catch(Exception e) {} 
+
+				now = System.nanoTime();
+			}
+		}
+	}
+
+	private void updateGame()
+	{
+		System.out.println("update");
+		panel.update();
+	}
+
+	private void drawGame(float interpolation)
+	{
+		System.out.println("draw");
+		panel.setInterpolation(interpolation);
+		panel.repaint();
 	}
 
 	private class GamePanel extends JPanel {
+		float interpolation;
 
 		public GamePanel() {
+		}
 
+		public void setInterpolation(float interp)
+		{
+			interpolation = interp;
+		}
+
+		public void update()
+		{
+			main.runSimulation();
 		}
 
 		public void drawLanes() {
-			System.out.println("here");
 			main.read(8);
 			JPanel tempPanel;
 			Insets insets = panel.getInsets();
@@ -219,14 +277,40 @@ public class FirstWindow {
 			if (!added)
 				lanes.add(tempPanel);
 			added = true;
-			System.out.println(lanes.size());
 		}
 
+		public void drawCars(Graphics2D g) {
+			int i, j;
+			ArrayList<Lane> lanes2= new ArrayList<Lane>();
+			ArrayList<Car> cars= new ArrayList<Car>();
+			for(int counter = 0; counter < 10; counter ++) {
+				lanes2 = main.getLanes();
+				for( i = 0; i< lanes.size(); i++) {
+					lanes.get(i).removeAll();
+					cars = lanes2.get(i).getCars();
+					for ( j = 0; j < cars.size(); j++) {
+						Insets insets = lanes.get(i).getInsets();
+						g.setColor(Color.red);
+						g.fillRect(insets.top + 5*(lanes2.get(i).getDistance() - cars.get(j).getCurrent_pos()) , insets.bottom,
+								cars.get(j).getCar_size()*5, lanes.get(i).getHeight());
+						JLabel temp = new JLabel("Car");
+						temp.setForeground(Color.red);
+						temp.setBounds(insets.left + 5*(lanes2.get(i).getDistance() - cars.get(j).getCurrent_pos()) , insets.top,
+								cars.get(j).getCar_size()*5, lanes.get(i).getHeight());
+						lanes.get(i).add(temp);
+					}
+				}
+			}
+		}
+		
+		@Override
 		public void paintComponent(Graphics g)
 		{	
 			super.paintComponent(g);
+			Graphics2D g2d = (Graphics2D) g;
 			drawLanes();
+			drawCars(g2d);
+			g.fillRect(lanes.get(0).getInsets().left, lanes.get(0).getInsets().top, 30, 30);
 		}
 	}
-
 }
